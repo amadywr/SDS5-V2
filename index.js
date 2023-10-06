86c155f630ee588d840473e6695bebb836858ee7
Ahmad Yawari <k_han21@yahoo.com>
2023-10-06 14:29:33 +1100

    a

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(methodOverride('_method'))
 // Passport Config
 require('./config/passport')(passport)
 
-// Connect to MongoDB
+// Connect to MongoDB aaa
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
